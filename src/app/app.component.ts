c1fcfbdbda3d99512be4f0d5d5f0093ea226bf9e
Finoana-Velia <fypatrick20@yahoo.com>
2025-05-12 09:47:27 +0300

    chore : install swiper js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  schemas : [CUSTOM_ELEMENTS_SCHEMA],
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
