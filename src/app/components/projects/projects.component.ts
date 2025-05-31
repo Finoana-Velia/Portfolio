24d70d5cbd642d0de9b8c4c0b7e8f05ee8cbd7b9
Finoana-Velia <fypatrick20@yahoo.com>
2025-05-31 08:43:18 +0300

    feat : update project component add new image for project update timeline

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,6 +13,8 @@ export class ProjectsComponent implements OnInit{
   projects = [
     {
       title : "Lisiniarivo Clothing and store",
+      background : "Projects/homes/lisiniarivo.png",
+      monitor : "Projects/details/lisiniarivo-web.png",
       description : "C'était mon premier contrat en freelance, la société Lisiniarivo Clothing and store m'a proposer de développer une application permettant de gérer leurs articles, leurs livreurs et leurs commandes mais aussi avec d'autres fonctionnalités supplémentaires comme la gestion de l'authentification et sécurité, l'affichage et la consultation des commandes à partir d'un calendri et enfin l'automatisation des intégration et le déploiement",
       technologies : [
         {
@@ -42,35 +44,10 @@ export class ProjectsComponent implements OnInit{
       ]
     },
 
-    // {
-    //   title : "ORA",
-    //   description : "ORA était à la base un dispositif destiné à enregistrer les entrées et sorties des employés d'une organisation. C'est par la suite que l'entreprise MDG-IT à decider de l'améliorer en lui intégran une interface administrateur mais aussi d'avoir le contrôle sur les activités professionnelles de la totalité des salariés voir même de plusieurs sociétés en même temps",
-    //   technologies : [
-    //     {
-    //       title : "Spring boot",
-    //       logo : "Logos/spring-boot-logo_full.png",
-    //     },
-    //     {
-    //       title : "Angular",
-    //       logo : "Logos/angular_icon-logo.png",
-    //     },
-    //     {
-    //       title : "Bootstrap",
-    //       logo : "Logos/Bootstrap_logo.svg.png",
-    //     },
-    //     {
-    //       title : "PostgreSQL",
-    //       logo : "Logos/Postgresql_elephant.svg.png",
-    //     },
-    //     {
-    //       title : "Git",
-    //       logo : "Logos/Git_icon.svg.png",
-    //     },
-    //   ]
-    // },
-
     {
       title : "RestFood and Bakery",
+      background : "Projects/homes/restfood&bakery.png",
+      monitor : "Projects/details/bakery-web-mobile.png",
       description : "RestFood & Bakery est une application de restauration permettant au utilisateur de passer des commandes en ligne selon son choix dans le menu, menu qui est géré à parti d'un espace administrateur et sécuriser. RestFood possède aussi une partie mobile en cours de développement permettant aux simple utilisateur d'utiliser leur smartphone comme support que se soit sur android ou sur IOS",
       technologies : [
         {
@@ -102,6 +79,8 @@ export class ProjectsComponent implements OnInit{
 
     {
       title : "Gasy Tube",
+      background : "Projects/homes/gasytube.png",
+      monitor : "Projects/details/gasytube-web-mobile.png",
       description : "C'est une application permettant de consulter un catalogue de film à partir de son smartphone avec le titre ainsi les acteurs qui y participe Toutes les données sont gérer par une interface web pour effectuer les actions de CRUDs, c'est en développant ce projets que j'ai fait mes premiers pas dans le développement Full Stack",
       technologies : [
         {
