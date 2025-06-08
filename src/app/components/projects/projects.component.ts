44ea842c7e414a5eefa4f74e11f4ff409f8d7282
Finoana-Velia <fypatrick20@yahoo.com>
2025-06-08 13:38:55 +0300

    translate project description and add git link's repos

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -17,7 +17,8 @@ export class ProjectsComponent implements OnInit,AfterViewInit{
       title : "Clothing and store",
       background : "Projects/homes/lisiniarivo.png",
       monitor : "Projects/details/lisiniarivo-web.png",
-      description : "This project is an application for managing delivery items and online orders for a clothing retail company. It also includes other features such as authentication and security, displaying and viewing orders from a calendar, all of which are integrated and deployed automatically.",
+      //description : "This project is an application for managing delivery items and online orders for a clothing retail company. It also includes other features such as authentication and security, displaying and viewing orders from a calendar, all of which are integrated and deployed automatically.",
+      description : "Ce projet est une application de gestion des articles livrés et des commandes en ligne pour une entreprise de vente de vêtements. Elle inclut également d'autres fonctionnalités telles que l'authentification et la sécurité, ainsi que l'affichage et la consultation des commandes à partir d'un calendrier, toutes intégrées et déployées automatiquement.",
       url : "",
       technologies: [
         {
@@ -44,14 +45,16 @@ export class ProjectsComponent implements OnInit,AfterViewInit{
           title : "Docker",
           logo : "Logos/free-docker-logo-icon.png",
         },
-      ]
+      ],
+      repos : "https://github.com/Finoana-Velia/Lisiniarivo-back.git",
     },
 
     {
       title : "RestFood and Bakery",
       background : "Projects/homes/restfood&bakery.png",
       monitor : "Projects/details/bakery-web-mobile.png",
-      description : "RestFood & Bakery is a restaurant application that allows users to place orders online according to their choice from the menu, a menu that is managed from a secure administrator space. RestFood also has a mobile part currently under development that allows ordinary users to use their smartphone as a support whether on Android or IOS.",
+      //description : "RestFood & Bakery is a restaurant application that allows users to place orders online according to their choice from the menu, a menu that is managed from a secure administrator space. RestFood also has a mobile part currently under development that allows ordinary users to use their smartphone as a support whether on Android or IOS.",
+      description : "RestFood & Bakery est une application de restauration permettant aux utilisateurs de commander en ligne selon leur choix à la carte, gérée depuis un espace administrateur sécurisé. RestFood propose également une version mobile en cours de développement permettant aux utilisateurs d'utiliser leur smartphone, qu'il soit sous Android ou iOS.",
       url : "",
       technologies : [
         {
@@ -78,14 +81,16 @@ export class ProjectsComponent implements OnInit,AfterViewInit{
           title : "Git",
           logo : "Logos/Git_icon.svg.png",
         },
-      ]
+      ],
+      repos : "https://github.com/Finoana-Velia/Recipe.git"
     },
 
     {
       title : "Gasy Tube",
       background : "Projects/homes/gasytube.png",
       monitor : "Projects/details/gasytube-web-mobile.png",
-      description : "It is an application allowing you to consult a film catalog from your smartphone with the title and the actors who participate in it. All the data is managed by a web interface to carry out CRUD actions.",
+      //description : "It is an application allowing you to consult a film catalog from your smartphone with the title and the actors who participate in it. All the data is managed by a web interface to carry out CRUD actions.",
+      description : "Il s'agit d'une application permettant de consulter un catalogue de films depuis votre smartphone, avec le titre et les acteurs qui y participent. Toutes les données sont gérées par une interface web pour effectuer des actions CRUD.",
       url : "",
       technologies : [
         {
@@ -108,7 +113,8 @@ export class ProjectsComponent implements OnInit,AfterViewInit{
           title : "Git",
           logo : "Logos/Git_icon.svg.png",
         },
-      ]
+      ],
+      repos : "https://github.com/Finoana-Velia/Movie.git"
     },
   ];
 
